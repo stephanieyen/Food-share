752dfb97ec7f1dc81486de7c7c9d1bde78c56e50
wafi <ishmam963@gmail.com>
2020-07-29 11:01:23 -0400

    modified app.py

--- app.py
+++ app.py
@@ -62,19 +62,23 @@ def receive():
         return render_template('receive.html', time=datetime.now())
     else:
         # this is storing the data from the form 
+        name = request.form['name'] 
         city = request.form['city'].lower()
-        print(city)
+        # print(city)
         typeoffood = request.form['type']
         email = request.form['email']
+        # this is connecting to mongodb
+        receive = mongo.db.receive
         # this is connecting to mongodb
         donate = mongo.db.donate
         donateView = list(donate.find({'city': city, 'type': typeoffood})) # sort?
         if len(donateView) > 0:
+            receive.insert({'name': name, 'city': city, 'type': typeoffood, 'email': email})
             message = "You got some results!"
         else:
             message = "Sorry, we couldn't find any results."
         print(donateView)
-        return render_template('receive.html', time=datetime.now(), donateView=donateView, message=message, email = email)
+        return render_template('receive.html', time=datetime.now(), donateView=donateView, message=message)
 
 # @app.route('/deleteall')
 # def deleteall():
@@ -105,12 +109,17 @@ def check_availability(description):
         donateview = list(donate.find({"description": description}))
         # print(donateview)
         quantity = donateview[0]["quantity"]
+        kind = donateview[0]["type"]
         # print(quantity)
         # subtraction = subtract user_quantity from quantity
         remaining = int(quantity) - int(user_quantity)
         print(remaining)
         # compare it with the quantity that's already in the database
         if remaining > 0:
+            # insert new data
+            receive = mongo.db.receive
+            searchreceive = list(receive.find({"type": kind}))
+            receive.insert({'quantity': quantity})
             donate.update({'description': description}, {"$set": {'quantity': str(remaining)}})
             donate = mongo.db.donate
             donateview = list(donate.find({"description": description}))
@@ -123,6 +132,8 @@ def check_availability(description):
             print(quantity)
             return render_template('message.html', time=datetime.now(), quantity = quantity, food = food, kind = kind, name = name, email = email)
         elif remaining == 0:
+            # insert new data
+            receive.insert({'quantity': quantity})
             donate = mongo.db.donate
             donate.remove({"description": description})
             return render_template('receive.html', time=datetime.now())
